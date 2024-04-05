1ce0ebd878df55299e624c4e8611ab4db953e0e4
Jamie Hogg <jamie-hogg@hotmail.com>
2024-04-05 10:58:22 +1000

    WORKING APP with static images

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import numpy as np
 
-# Create your views here.
+def home(request):
+
+    # create context dictionary
+    context = {}
+
+    if request.method == 'POST':
+        # Retrieve numbers from the form
+        context['x'] = float(request.POST.get('x', 0.0))
+        context['out'] = np.full(10, context['x']).sum()
+
+    return render(request, 'app/home.html', context)
+
